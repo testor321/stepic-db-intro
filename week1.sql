b4c0e7a39663b61a5f4413ea01509272b21070fa
testor321 <testor321@users.noreply.github.com>
2019-01-29 21:26:25 +0100

    20190129 update

--- week1.sql
+++ week1.sql
@@ -70,5 +70,102 @@ DELETE FROM billing
 WHERE (payer_email is null or payer_email = '') or
       (recipient_email is null or recipient_email = '');
       
+use project_simple;
+select * from project;
 
+-- Число проектов в базе
+SELECT count(project_name) FROM project;
+
+-- Средний бюджет
+SELECT avg(budget) FROM project;
+
+-- Сколько в дней уходит на выполнение проектов
+SELECT
+	project_name,
+    project_start,
+	project_finish,
+    datediff(project_finish, project_start)
+FROM project WHERE project_finish > project_start;
+
+-- Сколько дней в среднем
+SELECT avg(datediff(project_finish, project_start)) FROM project
+	WHERE project_finish > project_start;
+
+-- Минимальная и максимальная длительность проекта в днях
+SELECT
+	min(datediff(project_finish, project_start)),
+	max(datediff(project_finish, project_start))
+FROM project WHERE project_finish > project_start;
+
+-- Группировка
+SELECT
+	client_name,
+    min(datediff(project_finish, project_start)) AS min_days,
+    avg(datediff(project_finish, project_start)) AS avg_days,
+    max(datediff(project_finish, project_start)) AS max_days
+FROM project WHERE project_finish > project_start
+GROUP BY client_name
+ORDER BY max_days DESC;
+
+-- Выведите общее количество заказов компании.
+SELECT count(project_name) FROM project;
+
+-- Выведите в качестве результата одного запроса общее количество заказов,
+-- сумму стоимостей (бюджетов) всех проектов, средний срок исполнения заказа в днях.
+SELECT
+	count(project_name),
+    sum(budget),
+    avg(datediff(project_finish, project_start)) as duration
+FROM project;
     
+use store_simple;
+
+-- Сколько различных товаров в каждой категории
+SELECT
+	category,
+    count(product_name) AS quantity
+FROM store
+GROUP BY category
+ORDER BY quantity DESC;
+
+-- Общая выручка магазина
+SELECT
+	sum(price*sold_num) AS revenue
+FROM store;
+
+-- Наиболее популярные категории товаров
+SELECT
+	category,
+    sum(sold_num) AS sold
+FROM store
+GROUP BY category
+ORDER BY sold DESC;
+
+-- 10 наиболее популярных товаров
+SELECT
+	product_name,
+    sold_num
+FROM store
+ORDER BY sold_num DESC
+LIMIT 10;
+
+-- Выведите количество товаров в каждой категории, результат должен содержать два столбца:
+--	 название категории
+--	 количество товаров в данной категории
+SELECT
+	category,
+	count(product_name)
+FROM store
+GROUP BY category;
+
+-- Выведите 5 категорий товаров, продажи которых принесли наибольшую выручку.
+-- Под выручкой понимается сумма произведений стоимости товара на количество проданных единиц. Результат должен содержать два столбца: 
+--	название категории
+--	выручка от продажи товаров в данной категории
+SELECT
+	category,
+    sum(price*sold_num) AS revenue
+FROM store
+GROUP BY category
+ORDER BY revenue DESC
+LIMIT 5;
