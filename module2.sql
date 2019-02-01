7b5b451eb0065eacd92073fa99904d1634e3430c
testor321 <testor321@users.noreply.github.com>
2019-02-01 22:21:12 +0100

    Update module2.sql

--- module2.sql
+++ module2.sql
@@ -1,1 +1,45 @@
--- 
+USE store_medium;
+
+SELECT * FROM product;
+SELECT * FROM category;
+
+-- Декартово произведение
+SELECT * FROM product, category;
+SELECT * FROM product CROSS JOIN category;
+SELECT count(*) FROM product, category;
+
+-- Вывод списка продуктов с наименованием категории для каждого
+SELECT 
+    p.product_name, p.price, c.category_name
+FROM
+    product AS p
+        INNER JOIN
+    category AS c ON p.category_id = c.category_id;
+    
+-- Внешние соединения
+SELECT * FROM category AS c
+	LEFT OUTER JOIN product AS p ON c.category_id = p.category_id;
+SELECT * FROM product AS p
+	RIGHT OUTER JOIN category AS c ON c.category_id = p.category_id;
+
+-- Выборка из нескольких источников и объединение
+-- Самые дорогие и самые дешевые товары в БД
+SELECT * FROM product WHERE price > 900
+	UNION SELECT * FROM product WHERE price < 100;
+
+
+use store;
+
+-- Выведите все позиций списка товаров принадлежащие какой-либо категории
+-- с названиями товаров и названиями категорий. Список должен быть отсортирован
+-- по названию товара, названию категории.
+SELECT good.name, category.name FROM category_has_good
+	INNER JOIN good ON category_has_good.good_id = good.id
+    INNER JOIN category ON category_has_good.category_id = category.id
+    ORDER BY good.name, category.name;
+    
+-- Выведите список клиентов (имя, фамилия) и количество заказов данных клиентов, имеющих статус "new"
+SELECT client.first_name, client.last_name, count(1) AS new_sale_num FROM client
+	INNER JOIN sale ON client.id = sale.client_id
+    INNER JOIN status ON sale.status_id = status.id WHERE status.name = "new"
+    GROUP BY client.first_name, client.last_name;
