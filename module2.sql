ad650b5252ee9a14022d2d810fc49a4982ac10f0
testor321 <testor321@users.noreply.github.com>
2019-02-15 21:29:33 +0100

    module2 update

--- module2.sql
+++ module2.sql
@@ -101,3 +101,88 @@ SELECT category.name, count(sale.id) FROM category
     LEFT OUTER JOIN sale_has_good ON good.id = sale_has_good.good_id
     LEFT OUTER JOIN sale ON sale.id = sale_has_good.sale_id
     GROUP BY category.name;
+    
+-- Выведите список источников, из которых не было клиентов, либо клиенты пришедшие из которых не совершали
+-- заказов или отказывались от заказов. Под клиентами, которые отказывались от заказов, необходимо понимать
+-- клиентов, у которых есть заказы, которые на момент выполнения запроса находятся в состоянии 'rejected'.
+SELECT source.name FROM source
+	WHERE NOT EXISTS (SELECT * FROM client WHERE client.source_id = source.id)
+UNION
+SELECT source.name FROM source
+	INNER JOIN client ON client.source_id = source.id
+    INNER JOIN sale ON sale.client_id = client.id
+    INNER JOIN status ON status.id = sale.status_id WHERE status.name = "rejected";
+
+-- Источники из которых не было хороших клинетов
+SELECT source.name FROM source
+	WHERE NOT EXISTS (
+		SELECT * FROM client
+		INNER JOIN sale ON sale.client_id = client.id
+		INNER JOIN status ON status.id = sale.status_id WHERE status.name != "rejected" AND client.source_id = source.id
+);
+
+
+-- triggers
+-- prepare
+
+SELECT * FROM store.sale;
+SELECT * FROM store.sale_history;
+
+ALTER TABLE `sale` MODIFY id INTEGER AUTO_INCREMENT;
+
+ALTER TABLE `sale_history` MODIFY id INTEGER AUTO_INCREMENT;
+ALTER TABLE `sale_history` MODIFY active_from DATETIME;
+ALTER TABLE `sale_history` MODIFY active_to DATETIME;
+
+INSERT INTO `status` VALUES (0, "new");
+INSERT INTO `status` VALUES (1, "processing");
+INSERT INTO `status` VALUES (2, "ready");
+
+INSERT INTO `source` VALUES (0, "search");
+
+
+INSERT INTO `client` VALUES (0, "vasya_pupkin", "Vasily", "Pupkin", 0);
+
+INSERT INTO `sale` (client_id, number, dt_created, dt_modified, sum, status_id) 
+	VALUES (0, "cl_0_0", NOW(), NOW(), 195.23, 0);
+UPDATE `sale` SET status_id = 1 WHERE id = 1;
+UPDATE `sale` SET status_id = 2, sum = 212.35 WHERE id = 1;
+
+
+DROP PROCEDURE IF EXISTS update_order_history;
+DELIMITER //
+CREATE PROCEDURE update_order_history (
+  sale_id_ INTEGER,
+  status_id_ INTEGER,
+  sale_sum_ DECIMAL(18,2)
+)
+BEGIN
+  DECLARE now DATETIME;
+  SET now = NOW();
+  
+  UPDATE sale_history 
+    SET active_to = now 
+  WHERE sale_id = sale_id_ AND
+      #active_to IS NULL  
+          active_to = '2000.01.01'
+  LIMIT 1;
+
+  INSERT INTO sale_history
+         (sale_id, status_id, sale_sum, active_from, active_to)
+    #VALUES (sale_id_, status_id_, sale_sum_, now, NULL);
+    VALUES (sale_id_, status_id_, sale_sum_, now, '2000.01.01');        
+END
+//
+
+-- update_order_history
+DROP TRIGGER IF EXISTS on_update_order;
+DELIMITER //
+CREATE TRIGGER on_update_order
+  AFTER UPDATE
+  ON sale FOR EACH ROW
+BEGIN
+  CALL update_order_history(NEW.id, NEW.status_id, NEW.sale_sum);
+END
+//
+
+CALL update_order_history(56, 1, 1200);
